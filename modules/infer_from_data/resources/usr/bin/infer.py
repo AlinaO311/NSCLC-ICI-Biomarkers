c25a96b225a82095e2cc7d81033ea520675f47ba
AlinaO311 <alina.orozco12@gmail.com>
2024-06-17 14:43:07 +0200

    add cwd for infer.py , might need to change back

--- modules/infer_from_data/resources/usr/bin/infer.py
+++ modules/infer_from_data/resources/usr/bin/infer.py
@@ -2,18 +2,24 @@
 
 import os
 import argparse
+import glob
 from pathlib import Path
 from typing import Optional
 
 from trainer import Trainer
 
+cwd=os.getcwd().split('work', 1)[0]
+
 
 def main(experiment_folder: Path, data_path: Path, output_file: Path) -> None:
     assert output_file.suffix == ".csv", "ERROR: Output file does not have `.csv` extension! Exiting."
     model_config_path = experiment_folder
+    print('model_config_path ', model_config_path)
     pathlist = os.path.dirname(experiment_folder).split('/')[:-1]
+    print('pathlist', pathlist)
     pathlist = ['/' if x == '' else x for x in pathlist]
-    model_path = os.path.join(*pathlist, 'model')
+    model_path = os.path.join(cwd, *pathlist, 'model')
+    print('model_path', model_path)
     trainer = Trainer(model_config_path, resume_model=model_path, data_path=data_path)
     trainer.predict(output_file)
 
