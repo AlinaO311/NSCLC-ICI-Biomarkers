211c875509e97c93a424e6fee835937b4abc78f7
AlinaO311 <alina.orozco12@gmail.com>
2024-10-09 17:37:34 +0200

    add shap_tree_explainer to plots.py

--- bin/src/plots.py
+++ bin/src/plots.py
@@ -11,9 +11,11 @@ import numpy as np
 import pandas as pd
 import seaborn
 import sklearn
+import shap
 from matplotlib import colors as mcolors
 from matplotlib.ticker import MaxNLocator
 from sklearn.manifold import TSNE
+from collections import defaultdict
 
 # Constants.
 PLT_COLORS_BASE = list(dict(mcolors.BASE_COLORS).keys())
@@ -110,6 +112,42 @@ def scatter_plot(
     ax.set_xlabel(x_column, fontsize=14)
     ax.set_ylabel(y_column, fontsize=14)
     ax.legend()
+
+def shap_tree_explainer(
+    X_data: pd.DataFrame,
+    model : Any,
+    plotargs: Dict[str, Any] = {},
+) -> None:
+    """Generates a SHAP tree explainer plot for best model.
+
+    Arguments:
+        X_test -- a data frame containing the values to plot
+        model  -- the model to be explained
+
+    Keyword Arguments:
+        category_column -- The name of a column containing categories
+            for the plot color of each data point. (default: {None})
+        plotargs -- Any additional arguments for the scatter plot.
+
+    Returns:
+        The resulting plot as a seaborn facet grid.
+    """
+    groups = defaultdict(list)
+    one_hot_cols = X_data.columns[(X_data.isin([0, 1]).all())]
+    for col in one_hot_cols:
+        prefix = col.split('_')[0]
+        groups[prefix].append(col)
+
+    # Step 2: Create a copy of X_data to X_test
+    X_test = X_data.copy()
+    # Step 3: Drop the one-hot encoded columns from X_test
+    X_test.drop(columns=one_hot_cols, inplace=True)
+
+    for category, columns in groups.items():
+        X_test[f'{category}'] = X_data[columns].sum(axis=1)
+
+    shap_values = shap.TreeExplainer(model).shap_values(X_data)
+    shap.summary_plot(shap_values, X_data, show=False)
 
 
 def stacked_bar_plot(
