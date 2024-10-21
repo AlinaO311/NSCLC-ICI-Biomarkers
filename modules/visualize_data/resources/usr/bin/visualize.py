34c48d9494b251496f3ede8ff89edf2c71b12efd
AlinaO311 <alina.orozco12@gmail.com>
2024-10-21 14:25:19 +0200

    correct output path for visualizer

--- modules/visualize_data/resources/usr/bin/visualize.py
+++ modules/visualize_data/resources/usr/bin/visualize.py
@@ -9,10 +9,11 @@ from visualizer import Visualizer
 
 cwd=os.getcwd().split('work', 1)[0]
 
-def main(dir: Path, data_path: Optional[Path] = None) -> None:
-    output_path = os.path.join(cwd, dir ,"DataPrep" ,"visualization" )
+def main(dir: Path, mutation_data: Path,  data_path: Optional[Path] = None) -> None:
+    output_path = os.path.join(cwd, dir ,"DataPrep")
     visualizer = Visualizer(
-        output_path,
+        output_path=output_path,
+        mutfile=mutation_data, 
         data_path=data_path,
     )
     visualizer.visualize()
@@ -30,6 +31,7 @@ if __name__ == "__main__":
         type=Path,
         help="Path to the data on which to run the analysis.",
     )
+    parser.add_argument('--mutation_data', help='File of mutations of interest', required=False, default='mutations.txt')
     parser.add_argument("--dir", type=Path, help="Path to the output folder." )
 
     args = vars(parser.parse_args())
