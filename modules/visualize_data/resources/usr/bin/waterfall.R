f127013799cd9706a24c7a919e4e667a29b30369
AlinaO311 <alina.orozco12@gmail.com>
2024-11-18 07:39:39 +0100

    add paths to read files

--- modules/visualize_data/resources/usr/bin/waterfall.R
+++ modules/visualize_data/resources/usr/bin/waterfall.R
@@ -13,7 +13,7 @@ library(GenVisR)
 options <- list(
   make_option(c("-i", "--data_path"),help="Dataset for building model" ),
   make_option(c("-m", "--mutation_data"),help="File containing list of mutations"), 
-  make_option(c("-o", "--outputdir"),help="File containing patient data"),
+  make_option(c("-o", "--outputdir"),help="File containing patient data")
 )
 
 # Get the current working directory and split at 'work'
@@ -21,10 +21,6 @@ cwd <- str_split(getwd(), "work", n = 2)[[1]][1]
 
 # Set directory path and time_val from environment variable
 time_val <- Sys.getenv("time_val")
-
-# Construct output paths
-output_path <- file.path(cwd, dir, "DataPrep")
-output_folder <- file.path(output_path, paste0("visualization_", time_val))
 
 ### Handle arguments
 
@@ -37,13 +33,17 @@ infile <- opt$data_path
 mutfile <- opt$mutation_data
 dir <- opt$outputdir
 
+# Construct output paths
+output_path <- file.path(cwd, dir, "DataPrep")
+output_folder <- file.path(output_path, paste0("visualization_", time_val))
+
 # Set the current working directory and file path
-mutData <- file.path(mutfile)
+mutData <- file.path(cwd, mutfile)
 
 set.seed(426)
 
 ###### Read patient data
-meta_tbl <- read.table(opt$infile, sep="\t",header=T)
+meta_tbl <- read.table(infile, sep="\t",header=T)
 
 ###### Read mutation data and clean the list
 mut_list <- scan(mutData, what = "", sep = ",", quiet = TRUE) # Split on commas
@@ -87,13 +87,15 @@ setnames(melted, c("sample", "gene", "variant_class", "counts"))
 ######################### ADD clinical data
 
 # Load the required library
-library(RColorBrewer)
+library(colorspace)
 
 # Number of unique mutation categories (you need this to match the number of colors)
 n_colors <- length(unique(melted$Mutation_Type))  # Assuming 'Mutation_Type' has the mutation categories
 
+palette <- qualitative_hcl(n_colors, palette = "Dark3")
+
 # Generate a palette with enough colors
-custom_palette <- brewer.pal(n = n_colors, name = "Set3")
+mutationColours <- setNames(palette, unique(melted$Mutation_Type))
 
 # Select clinical variables to include in plot - durable clinical benefit response map to responder and non-responder
 clinicalData <- meta_tbl %>% 
@@ -130,5 +132,5 @@ clinVarOrder_map <- c(unique(clinicalData$Best_Response[order(clinicalData$Best_
 output_file <- file.path(output_folder, "waterfall_plot.png")
 
 png(output_file, height=12, width=15, units="in", res=300)
-waterfall(melted, fileType = "Custom",  variant_class_order=mutation_priority , clinData=clinicalData_2,clinVarOrder=clinVarOrder_map, clinLegCol=ncol(clinicalData)-1, section_heights=c(1,5,1), mainRecurCutoff = 0.05, mutation_palette = custom_palette)
+waterfall(melted, fileType = "Custom",  variant_class_order=mutation_priority , clinData=clinicalData_2,clinVarOrder=clinVarOrder_map, clinLegCol=ncol(clinicalData)-1, section_heights=c(1,5,1), mainRecurCutoff = 0.05,   mainPalette=mutationColours)
 dev.off()
